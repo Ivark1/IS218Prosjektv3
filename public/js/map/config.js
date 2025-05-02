e3ac3abe7b065ae2cb3fcd0afdd40e881c8cfa6a
terjems <terjems@uia.no>
2025-05-02 16:51:25 +0200

    Lagt til sidebar med filter-knapper

--- public/js/map/config.js
+++ public/js/map/config.js
@@ -18,13 +18,16 @@ function initializeProjection() {
 // Initialize base map
 function initializeMap() {
     L.Icon.Default.imagePath = 'https://unpkg.com/leaflet@1.9.4/dist/images/';
-    const map = L.map('map').setView([58.163576619299235, 8.003306530291821], 13);
+    const map = L.map('map', {
+        zoomControl: false,
+    }).setView([58.163576619299235, 8.003306530291821], 13);
+    
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 19
     }).addTo(map);
-
+    L.control.zoom({ position: 'topright' }).addTo(map);
     return map;
 }
 
